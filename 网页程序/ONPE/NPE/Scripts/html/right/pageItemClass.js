a858b8808bd0ad392655a85acccefa9b2562f10e
hdm188717 <39142693+hdm188717@users.noreply.github.com>
2018-11-14 15:46:15 +0800

    cl

--- 网页程序/ONPE/NPE/Scripts/html/right/pageItemClass.js
+++
@@ -1,5 +0,0 @@
-﻿var pageItemClass = [{ "id": 0, "text": "页面" }, { "id": 1, "text": "按钮" },
-{ "id": 2, "text": "搜索框" },
-{ "id": 3, "text": "排序列" },
-{ "id": 4, "text": "下拉框" },
-{ "id": 5, "text": "选择树"}];
--- 网页程序/ONPE/NPE/Scripts/html/right/rightClass.js
+++
@@ -1,6 +0,0 @@
-﻿var rightClass = [
-{ "id": 1, "text": "PC端B/S权限",selected:true },
-{ "id": 2, "text": "PC端C/S权限" },
-{ "id": 3, "text": "移动端android权限" },
-{ "id": 4, "text": "移动端IOS权限" }
-];
